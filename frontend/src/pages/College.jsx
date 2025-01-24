3109a54ec4558dc7c8d1710c2650f7510cfa62aa
Jay Prakash <jayprakash1200@gmail.com>
2025-01-24 17:31:40 +0530

    Added college Path

--- frontend/src/pages/College.jsx
+++ frontend/src/pages/College.jsx
@@ -1,32 +1,14 @@
-import React from 'react'
-import DashbordNavbar from '../components/DashbordNavbar'
-import Sidebar from '../components/Sidebar'
-import CollegeCard from '../components/CollegeCard'
-import core from "../assets/Core.png"
-import attendance from "../assets/attendance.png"
-import database from "../assets/database.png"
-import todo from "../assets/todo.png"
-import { CORE_DESC,ATTENDANCE_DESC,DATABASE_DESC,TODO_DESC} from '../utils/constants'
-import { Link } from 'react-router-dom'
+import React from "react";
+import DashbordNavbar from "../components/DashbordNavbar";
+import Sidebar from "../components/Sidebar";
 
 function College() {
   return (
-    <div className="min-h-screen flex flex-col bg-black">
-      <DashbordNavbar label={"COLLEGE "} />
-      <div className="flex flex-grow">
-        <Sidebar />
-        <div className="flex-grow ml-[180px] p-8">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-20 ">
-            <Link to={"/dashboard/college/core"}><CollegeCard  image={core} label={"CORE"} description={CORE_DESC} ></CollegeCard></Link>
-            <Link to={"/dashboard/college/attendance"}><CollegeCard image={attendance} label={"ATTENDANCE"} description={ATTENDANCE_DESC} ></CollegeCard></Link>
-            <Link to={"/dashboard/college/database"}><CollegeCard image={database} label={"DATA BASE"} description={DATABASE_DESC}></CollegeCard></Link>
-            <Link to={"/dashboard/college/todo"}><CollegeCard  image={todo} label={"TO-DO"} description={TODO_DESC}></CollegeCard></Link>
-         
-          </div>
-        </div>
-      </div>
+    <div>
+      <DashbordNavbar label={"COLLEGE"}></DashbordNavbar>
+      <Sidebar></Sidebar>
     </div>
-  )
+  );
 }
 
-export default College
+export default College;
